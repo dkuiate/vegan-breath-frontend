0d17c77f6d8a33251fdb541f6e3c48f55adc8519
ClydeBe <56868638+ClydeBe@users.noreply.github.com>
2020-09-08 01:30:02 +0200

    navbar first try

--- BreatheVeganFront/breathevegan/src/app/app.module.ts
+++ BreatheVeganFront/breathevegan/src/app/app.module.ts
@@ -3,15 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {WelcomeModule} from './module/welcome/welcome.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        WelcomeModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- BreatheVeganFront/breathevegan/src/app/module/welcome/welcome.module.ts
+++ BreatheVeganFront/breathevegan/src/app/module/welcome/welcome.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterModule} from "@angular/router";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
 
 
 
 @NgModule({
-  declarations: [WelcomePageComponent],
+    declarations: [WelcomePageComponent],
+    exports: [
+        WelcomePageComponent
+    ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule,
+    MatSidenavModule,
+    MatListModule
   ]
 })
 export class WelcomeModule { }
