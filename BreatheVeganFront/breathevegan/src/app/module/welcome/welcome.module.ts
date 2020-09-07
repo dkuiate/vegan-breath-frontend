27b6eba56bcad898d8fe4f6522e014aadd4ac745
ClydeBe <56868638+ClydeBe@users.noreply.github.com>
2020-09-08 01:30:45 +0200

    navbar first try

--- BreatheVeganFront/breathevegan/src/app/module/welcome/welcome.module.ts
+++ BreatheVeganFront/breathevegan/src/app/module/welcome/welcome.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {RouterModule} from "@angular/router";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterModule} from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 
 
