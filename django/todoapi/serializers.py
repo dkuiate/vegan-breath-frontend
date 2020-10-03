01543d7144fe6e0844ef495fbaefc09916032da6
brice <brice.william971@gmail.com>
2020-10-03 14:43:08 +0200

    remove django folder

--- django/todoapi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Recepee
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Recepee
-        fields = ('title', 'content', 'created_on',  'due_date')
