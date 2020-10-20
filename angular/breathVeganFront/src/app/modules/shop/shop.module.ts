45faf1e257ed5cb1667e8f23f2f6edc094b41fc2
dkuiate <danelkatel.kuiatetagne@canal-plus.com>
2020-10-20 10:06:58 +0200

    project struture, auth

---
+++ angular/breathVeganFront/src/app/modules/shop/shop.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShopListComponent } from './shop-list/shop-list.component';
+
+
+
+@NgModule({
+  declarations: [ShopListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShopModule { }
