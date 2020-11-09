851a1406cba4c5bcf75253770d3b97c466f1e33e
dkuiate <danelkatel.kuiatetagne@canal-plus.com>
2020-11-09 14:11:37 +0100

    adding lazing-loading + routing

--- angular/breathVeganFront/src/app/app-routing.module.ts
+++ angular/breathVeganFront/src/app/app-routing.module.ts
@@ -1,33 +1,49 @@
 import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Component, NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AuthGuard } from './apiServices/auth.service';
 import { LogInComponent } from './modules/registration/log-in/log-in.component';
 import { SignupComponent } from './modules/registration/signup/signup.component';
 import { ShopListComponent } from './modules/shop/shop-list/shop-list.component';
 
 
+
 const routes: Routes = [
   {
-    path: 'login',
-    component: LogInComponent
+    path: 'registration',
+    component: SignupComponent
     /*loadChildren: () => import('./modules/registration/registration.module').then(
       m => m.RegistrationModule 
     )*/
+  
   },
   {
-    path: 'signup',
-    component: SignupComponent
+    path: 'lazyRegistration',
+    loadChildren: './modules/registration/registration-routing.module#RegistrationRoutingModule'
   },
   {
-    path: 'shop-list',
-    component: ShopListComponent,
+    path: 'home',
+    loadChildren: './modules/home/home-routing.module#HomeRoutingModule',
     canActivate: [AuthGuard]
   },
+  {
+    path:'recepee',
+    loadChildren:'/modules/recepee/recepee-routing.module#RecepeeRoutingModule'
+  },
+  {
+    path:'shop',
+    loadChildren: '/modules/shop/shop-routing.module#ShopRoutingModule'
+  },
+  {
+    path:'resteurants',
+    loadChildren: './modules/restaurants/restaurants-routing.module#RestaurantsRoutingModule'
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule],
   providers:[]
 })
