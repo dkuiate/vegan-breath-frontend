5bfb97ed193b2f7468e7aa67dd8666ca8372b5c7
dkuiate <64785971+dkuiate@users.noreply.github.com>
2021-01-07 23:20:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/breath-vegan-front'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/breath-vegan-front/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/breath-vegan-front/src/index.html'));
 });
 
 // default Heroku PORT
